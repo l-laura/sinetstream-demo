9b2ff2604d4a6c333f03d17e07fcc596d382af5e
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2022-11-07 18:17:39 +0900

    kafka-pcap: Log Kafka traffic to file from separate Docker container.

--- option/Server/Kafka/kafka-pcap/packet-capture/capture.sh
+++ option/Server/Kafka/kafka-pcap/packet-capture/capture.sh
@@ -1,1 +1,15 @@
-tcpdump -c 10 -w /pcap/packets.pcap "dst port 9092"
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+
+# -c: Exit after logging 10.000.000 packets. Assuming each packet is
+#     around 100 bytes, tcpdump exits after logging 1 GB data.
+# -G: A new logfile is created each minute.
+
+tcpdump \
+-c ${PCAP_MAX_PACKET_COUNT:-10000000} \
+-G 60 \
+-w /pcap/packets-%F-%T.pcap \
+"dst port ${DESTINATION_PORT}" \
+-i any \
+-Z root
